dac6ab398859c3ebb4c919ea996240bb79f759a9
MD. Mehedi Hasan Rabbi <mehedi.rabbi@asa-international.com>
2025-03-06 11:36:11 +0600

    Update project structure.

---
+++ src/Application/Mehedi.CleanArchitecture.Application/UseCases/Customers/Queries/GetAllCustomerQuery.cs
@@ -0,0 +1,7 @@
+﻿using CleanArchitecture.Domain.Aggregates.CustomerAggregate;
+using MediatR;
+using Mehedi.Application.SharedKernel.Responses;
+
+namespace Mehedi.CleanArchitecture.Application.UseCases.Customers.Queries;
+
+public record GetAllCustomerQuery : IRequest<Result<IEnumerable<CustomerQueryModel>>>;
---
+++ src/Presentation/Mehedi.CleanArchitecture.Api/Controllers/CustomerController.cs
@@ -0,0 +1,45 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using CleanArchitecture.Application.UseCases.Customers.Commands;
+using MediatR;
+using Mehedi.CleanArchitecture.Application.UseCases.Customers.Queries;
+using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Mime;
+
+namespace Mehedi.CleanArchitecture.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CustomersController(IMediator mediator) : ControllerBase
+{
+    private readonly IMediator _mediator = mediator;
+
+    /// <summary>
+    /// Register a new customer.
+    /// </summary>
+    /// <response code="200">Returns the Id of the new client.</response>
+    [HttpPost]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public async Task<IActionResult> Create([FromBody][Required] CreateCustomerCommand command)
+    {
+        await _mediator.Send(command);
+        return Ok("Customer Command Request Accepted");
+    }
+
+    /// <summary>
+    /// Get all customer data
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [Consumes(MediaTypeNames.Application.Json)]
+    [Produces(MediaTypeNames.Application.Json)]
+    public async Task<IActionResult> GetAll()
+    {
+        var result = await _mediator.Send(new GetAllCustomerQuery());
+
+        return Ok(result);
+    }
+}
