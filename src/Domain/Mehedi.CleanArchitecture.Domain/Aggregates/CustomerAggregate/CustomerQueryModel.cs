52c310811248535e882dfab083db88ee388163ad
MD. Mehedi Hasan Rabbi <mehedi.rabbi@asa-international.com>
2025-03-18 11:41:47 +0600

    Update CustomerQueryModel structure.

--- src/Domain/Mehedi.CleanArchitecture.Domain/Aggregates/CustomerAggregate/CustomerQueryModel.cs
+++ src/Domain/Mehedi.CleanArchitecture.Domain/Aggregates/CustomerAggregate/CustomerQueryModel.cs
@@ -3,9 +3,4 @@ using Mehedi.Core.SharedKernel;
 
 namespace CleanArchitecture.Domain.Aggregates.CustomerAggregate;
 
-public record CustomerQueryModel(Guid id, string name, PhoneNumber phone) : IQueryModel<Guid>
-{
-    public Guid Id { get; private init; } = id;
-    public string name { get; private init; } = name;
-    public PhoneNumber Phone { get; private init; } = phone;
-}
+public record CustomerQueryModel(Guid Id, string Name, PhoneNumber Phone) : IQueryModel<Guid>;
